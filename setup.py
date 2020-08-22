6252d2c4b696a658b75883a190fbd6c506f28790
Peter Stein <peterstein@protonmail.com>
2020-08-22 21:58:16 +0200

    Create script to download all blinks

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="dailyblink",
-    version="0.2.1",
+    version="0.4",
     url="https://github.com/ptrstn/daily-blinkist",
     packages=find_packages(),
     install_requires=["requests", "beautifulsoup4", "mutagen"],
-    entry_points={"console_scripts": ["dailyblink=dailyblink.blinks:main"]},
+    entry_points={
+        "console_scripts": [
+            "dailyblink=dailyblink.blinks:main",
+            "allblinks=dailyblink.blinkist:main",
+        ]
+    },
 )
