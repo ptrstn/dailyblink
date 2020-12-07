cdc02b70f3fc1efd4f426094837e2aeda5eddae0
Peter Stein <peterstein@protonmail.com>
2020-12-07 23:32:45 +0100

    Fix url in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="dailyblink",
     version="0.5.1",
-    url="https://github.com/ptrstn/daily-blinkist",
+    url="https://github.com/ptrstn/dailyblink",
     packages=find_packages(),
     install_requires=["requests", "beautifulsoup4", "mutagen", "cloudscraper"],
     entry_points={
