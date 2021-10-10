79c69773d157f12085dec75853fdd34185000bcc
Peter Stein <peterstein@protonmail.com>
2021-10-10 04:07:29 +0200

    Refactor version number inside package
    In this way, the version number is accessible within the package itself.

--- setup.py
+++ setup.py
@@ -1,8 +1,28 @@
+import codecs
+import os
+import re
+
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), "r") as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 
 setup(
     name="dailyblink",
-    version="0.5.2",
+    version=find_version("dailyblink", "__init__.py"),
     url="https://github.com/ptrstn/dailyblink",
     packages=find_packages(),
     install_requires=["requests", "beautifulsoup4", "mutagen", "cloudscraper"],
